0620e4a48edc1161fef61b481f8f6105f61b37e3
Александр <104736526+ROS-71@users.noreply.github.com>
2023-02-20 12:12:03 +0300

    Добавление настроек, корректировка ДЗ 2 раздела

--- js/functions.js
+++ js/functions.js
@@ -4,29 +4,31 @@
 // БОЛЬШЕ ФУНКЦИЙ
 
 // Первое задание:
-// Вариант 1 - стравнивает текстовую строку переданную аргументом и число не менее 10 согласно аргументу
-console.log('Задание № 1. Вариант 1');
-const getStringCheck = function (str, strLength) {
-  if (str === 'проверяемая строка' && strLength > 10) {
-    return `${true} - строка проходит по длине`;
-  } else {
 
-    return `${false} — строка не проходит`;
-  }
-};
-console.log(getStringCheck('проверяемая строка', 20));
-console.log(getStringCheck('проверяемая строка', 18));
-console.log(getStringCheck('проверяемая строка', 10));
+console.log('Задание № 1. Вариант 1');
+/**
+ * Проверяет вписывается ли строка в указанную длину
+ * @param {string} target
+ * @param {number} maxLength
+ * @return {boolean}
+ */
+const fitsLength = (target, maxLength) => target.length <= maxLength;
+
+console.log(fitsLength('проверяемая строка', 20));
+console.log(fitsLength('проверяемая строка', 18));
+console.log(fitsLength('проверяемая строка', 10));
 
 
 // Вариант 2 - проверяет length строки при условии, что она меньше или равна числу второго аргумента
 console.log('\nЗадание № 1. Вариант 2');
-const StringCheck = function (str, strLength) {
+const StringCheck = (str, strLength) => {
   if (str.length <= strLength) {
     return `${true} - строка проходит по длине`;
   } else {
     return `${false} — строка не проходит`;
   }
+
+
 };
 console.log(StringCheck('проверяемая строка', 20));
 console.log(StringCheck('проверяемая строка', 18));
@@ -36,47 +38,59 @@ console.log(StringCheck('проверяемая строка', 10));
 // Второе задание:
 // Вариант 1
 console.log('\nЗадание № 2. Вариант 1');
-function isPalindrome(wordPalindrome) {
-  let wordCheck = '';
-  for (let i = wordPalindrome.length - 1; i >= 0; --i) {
-    wordCheck += wordPalindrome[i];
-  }
-  return wordPalindrome == wordCheck;
-}
+/**
+ * Проверяет является ли строка (или число) палиндромом
+ * @param {string|number} target
+ * @return {boolean}
+ */
+const isPalindrome = (target) => {
+  const normalized = String(target).replaceAll(' ', '').toLowerCase();
+  const reversed = normalized.split('').reverse().join('');
+
+  return normalized === reversed;
+};
+
 console.log(isPalindrome('топот')); // Результат: true - строка является палиндромом
 console.log(isPalindrome('ДовОд')); // Результат: true - несмотря на разный регистр, тоже палиндром
 console.log(isPalindrome('Кекс')); // Результат: false - это не палиндром
 
-
-// Варинат 2
-console.log('\nЗадание № 2. Вариант 2. (Видит регистр символов)');
-const isPalindromeWord = function (wordPalindrome) {
-  wordPalindrome = wordPalindrome.toLowerCase().replace(/[^а-яa-z1-9]/gi, '');
-  const lastIndex = wordPalindrome.length - 1;
-  for (let i = 0; i < wordPalindrome.length / 2; i++) {
-    if (wordPalindrome[i] !== wordPalindrome[lastIndex - i]) {
-      return false;
-    }
-  }
-  return true;
-};
-console.log(isPalindromeWord('топот')); // Результат: true - строка является палиндромом
-console.log(isPalindromeWord('Дов0д')); // Результат: true - несмотря на разный регистр, тоже палиндром
-console.log(isPalindromeWord('Кекс')); // Результат: false - это не палиндром
-
-
 // Третье задание
-console.log('\nЗадание № 3.(не убирает точку в 0.5, как и не видит 0.5 вообще!');
-function isLeadNumber(stringNumber) {
-  const strNum = parseInt(stringNumber.match(/\d+/), 10);
-  return strNum;
-}
-console.log(isLeadNumber('2023 год')); // Результат: число 2023
-console.log(isLeadNumber('ECMAScript 2022')); // Результат: число 2022
-console.log(isLeadNumber('1 кефир, 0.5 батона')); // Результат: число 105
-console.log(isLeadNumber('а я томат')); // Результат: NaN
+console.log('\nЗадание № 3.');
+/**
+ * Извлечёт из строки (или числа) цифры от 0 до 9
+ * @param {string|number} target
+ * @return {number}
+ */
+const parseDigits = (target) => {
+  const digits = String(target).replace(/[^0-9]/g, '');
+
+  return digits ? Number(digits) : NaN;
+};
+
+console.log(parseDigits('2023 год')); // Результат: число 2023
+console.log(parseDigits('ECMAScript 2022')); // Результат: число 2022
+console.log(parseDigits('1 кефир, 0.5 батона')); // Результат: число 105
+console.log(parseDigits('а я томат')); // Результат: NaN
 
 
 // Четвёртое задание
 console.log('\nЗадание № 4. - В работе, изучаю спец статью для выполнения. DL 26.02.2023');
+/**
+ * Добавит в начало строки символы другой строки
+ * В результате исходная строка достигнет заданной длинны
+ * @param {string} target
+ * @param {number} length
+ * @param {string} pad
+ * @return {string}
+ */
+const padStart = (target, length, pad) => {
+  const start = pad.repeat(length);
 
+  return `${start}${target}`.slice(-length);
+};
+
+// Добавочный символ использован один раз
+console.log(padStart('1', 2, '0')); // Результат: строка '01'
+
+// Добавочный символ использован три раза
+console.log(padStart('1', 4, '0')); // Результат: строка '0001'
