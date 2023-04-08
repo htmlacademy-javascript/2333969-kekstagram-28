c6692873f3e63de4daf604c88c45e0356257e0ec
Александр <104736526+ROS-71@users.noreply.github.com>
2023-04-08 18:33:38 +0300

    Revert "Module8 task2"

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-// Подключаем функцию createImageStateList из файла data.js
-import createPictureStateList from './data.js';
+// Подключаем весь файл gallery.js
+import './gallery.js';
 
 // подключаем функцию renderPictures из файла gallery.js
 import initGallery from './gallery.js';
+
+// Подключаем функцию createImageStateList из файла data.js
+import createPictureStateList from './data.js';
 
 initGallery(createPictureStateList());
 
